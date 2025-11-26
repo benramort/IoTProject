96e2a763f6160582378f8aa86a63ac1aeccc77ef
Gr701 <mark.suvorov.gr@gmail.com>
2025-11-26 10:45:47 +0200

    create sensor.api template

---
+++ server/sensors/api.py
@@ -0,0 +1,43 @@
+
+def get_temperature():
+    return 10
+
+
+def get_light_level():
+    return 20
+
+
+def play_sound():
+    return
+
+
+def draw_screen():
+    return
+
+
+def get_lock_state():
+    return False
+
+
+def lock_bike():
+    return
+
+
+def unlock_bike():
+    return
+
+
+def get_light_state():
+    return False
+
+
+def turn_lights_on():
+    return
+
+
+def turn_lights_off():
+    return
+
+
+def get_gps():
+    return
